68f7bbd78b924d1b09772fda366996ac5eabeffa
Rain_Turner <1828113804@qq.com>
2021-12-10 00:52:08 +0800

    实现了基础功能

---
+++ main.ino
@@ -0,0 +1,100 @@
+/*
+
+  HelloWorld.ino
+
+  Universal 8bit Graphics Library (https://github.com/olikraus/u8g2/)
+
+  Copyright (c) 2016, olikraus@gmail.com
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification,
+  are permitted provided that the following conditions are met:
+
+    Redistributions of source code must retain the above copyright notice, this list
+    of conditions and the following disclaimer.
+
+    Redistributions in binary form must reproduce the above copyright notice, this
+    list of conditions and the following disclaimer in the documentation and/or other
+    materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include "utils.h"
+#include "Adafruit_SHT31.h"
+//#include "SHT31.h"
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+//U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+/*
+  U8g2lib Example Overview:
+    Frame Buffer Examples: clearBuffer/sendBuffer. Fast, but may not work with all Arduino boards because of RAM consumption
+    Page Buffer Examples: firstPage/nextPage. Less RAM usage, should work with all Arduino boards.
+    U8x8 Text Only Example: No RAM usage, direct communication with display controller. No graphics, 8x8 Text only.
+
+*/
+
+// Please UNCOMMENT one of the contructor lines below
+// U8g2 Contructor List (Frame Buffer)
+// The complete list is available here: https://github.com/olikraus/u8g2/wiki/u8g2setupcpp
+// Please update the pin numbers according to your setup. Use U8X8_PIN_NONE if the reset pin is not connected
+
+//SHT31 sht;
+
+
+void setup(void) {
+  sht31.begin(0x44);
+  u8g2.begin();
+  Serial.begin(9600);
+  //  Wire.begin();
+  //  sht.begin(0x44);
+}
+
+void loop(void) {
+  int temp = 20;
+  float val = lightDetect(A0);
+  float t = sht31.readTemperature();
+  float h = sht31.readHumidity();
+  u8g2.setFont(u8g2_font_ncenB08_tr);  // choose a suitable font
+  //  sht.read();
+  //  int tem = sht.getTemperature();
+  //  int hum = sht.getHumidity();
+  //  u8g2.clearBuffer();					// clear the internal memory
+  //  u8g2.sendBuffer();					// transfer internal memory to the display
+  u8g2.firstPage();
+  do {
+    /* all graphics commands have to appear within the loop body. */
+    u8g2.drawStr(0, 16, "Temperature   :           C");  // write something to the internal memory
+    u8g2.setCursor(85, 16);
+    u8g2.print(t,1);
+    u8g2.drawStr(0, 38, "Humidity       :           %");
+    u8g2.setCursor(85, 38);
+    u8g2.print(h,1);
+    u8g2.drawStr(0, 60, "Illumination :           %");
+    u8g2.setCursor(85, 60);
+    u8g2.print(val,1);
+//    Serial.println(h);
+  } while ( u8g2.nextPage() );
+  delay(1000);
+}
