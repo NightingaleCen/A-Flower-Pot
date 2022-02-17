8429803416bd7de8f033bb176f51a83620d075fa
NightingaleCen <nightingalecen@outlook.com>
2022-02-18 03:55:48 +0800

    新增了红外接收功能，重构了代码

--- main/main.ino
+++ main/main.ino
@@ -1,40 +1,35 @@
-#include <Arduino.h>
-#include <U8g2lib.h>
+#include <IRremote.hpp>
 
 #include "utils.h"
-#include "Adafruit_SHT31.h"
 
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
+U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
+
 unsigned long loopTime;
+int waterDuration = 2 * ONE_SEC;//初始灌溉时长为2秒
 
 void setup(void)
 {
   sht31.begin(SHT31_ADDR);
   u8g2.begin();
-  u8g2.drawXBMP(0, 0, 128, 64, cow);
-  u8g2.sendBuffer();
-  u8g2.clearBuffer();
+  IrReceiver.begin(RECV_PIN, ENABLE_LED_FEEDBACK);
+  loadTheCow(u8g2);//载入安科拉长角牛！
   loopTime = millis();
 }
 
 void loop(void)
 {
-  if (millis() - loopTime > TEN_SEC){
+  /*接收红外信号调整灌溉时长*/
+  if (IrReceiver.decode()) {
+    IrReceiver.resume();
+    if (millis() - loopTime > 5.2 * ONE_SEC) {
+      changeWaterDuration(waterDuration, IrReceiver.decodedIRData.command, u8g2);
+      loopTime = millis() - 5 * ONE_SEC;  
+    }
+  }
+  /*显示信息*/
+  else if (millis() - loopTime > 10 * ONE_SEC) {  //每十秒显示一次
+    displayInf(u8g2, sht31);
     loopTime = millis();
-    u8g2.setFont(u8g2_font_ncenB08_tr);
-    u8g2.firstPage();
-    do
-    {
-      u8g2.drawStr(0, 16, "Temperature   :           C");
-      u8g2.setCursor(85, 16);
-      u8g2.print(sht31.readTemperature(), 1); //显示温度
-      u8g2.drawStr(0, 38, "Humidity       :           %");
-      u8g2.setCursor(85, 38);
-      u8g2.print(humidityDetect(humiditySensorPin), 1); //显示湿度
-      u8g2.drawStr(0, 60, "Illumination :           %");
-      u8g2.setCursor(85, 60);
-      u8g2.print(lightDetect(lightSensorPin), 1); //显示环境光强
-    } while (u8g2.nextPage());
   }
 }
--- utils/utils.cpp
+++ utils/utils.cpp
@@ -1,22 +1,80 @@
 /*
  * utils.cpp
- * 提供花盆各项功能的库 
+ * 提供花盆各项功能的库
  */
 #include "Arduino.h"
 
 #include "Wire.h"
+#include "U8g2lib.h"
+#include "Adafruit_SHT31.h"
 #include "utils.h"
 
-//返回当前环境光照强度的百分比数值
+/*返回当前环境光照强度的百分比数值*/
 int lightDetect(const int sensorPin)
 {
     int val = analogRead(sensorPin);
     return (100 - map(val, 0, 1023, 0, 100));
 }
 
-//返回当前土壤湿度百分比数值
+/*返回当前土壤湿度百分比数值*/
 int humidityDetect(const int sensorPin)
 {
     int val = analogRead(sensorPin);
-    return (100 - map(val,humidityInWater,humidityInAir,0,100));
+    return (100 - map(val, HUMIDITY_IN_WATER, HUMIDITY_IN_AIR, 0, 100));
+}
+
+/*载入安科拉长角牛*/
+void loadTheCow(U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2)
+{
+
+    u8g2.firstPage();
+    do
+    {
+        u8g2.drawXBMP(0, 0, 128, 64, cow);
+    } while (u8g2.nextPage());
+}
+/*改变单次灌溉时长*/
+void changeWaterDuration(int &waterDuration, int signal, U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2)
+{
+    switch (signal)
+    {
+    case INCREASE_SIGNAL:
+        if (waterDuration < 10 * ONE_SEC)
+            waterDuration += ONE_SEC;
+        break;
+    case DECREASE_SIGNAL:
+        if (waterDuration > 1 * ONE_SEC)
+            waterDuration -= ONE_SEC;
+        break;
+    default:
+        return;
+    }
+    u8g2.setFont(u8g2_font_ncenB08_tr);
+    u8g2.firstPage();
+    do
+    {
+        u8g2.drawStr(0, 16, "Current Water Duration is:");
+        u8g2.setCursor(60, 32);
+        u8g2.print(waterDuration / ONE_SEC, 1);
+        u8g2.drawStr(42, 48, "Seconds");
+    } while (u8g2.nextPage());
+}
+
+/*显示信息*/
+void displayInf(U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2, Adafruit_SHT31 sht31)
+{
+    u8g2.setFont(u8g2_font_ncenB08_tr);
+    u8g2.firstPage();
+    do
+    {
+        u8g2.drawStr(0, 16, "Temperature   :           C");
+        u8g2.setCursor(85, 16);
+        u8g2.print(sht31.readTemperature(), 1); //显示温度
+        u8g2.drawStr(0, 38, "Humidity       :           %");
+        u8g2.setCursor(85, 38);
+        u8g2.print(humidityDetect(HUMIDITY_SENSOR_PIN), 1); //显示湿度
+        u8g2.drawStr(0, 60, "Illumination :           %");
+        u8g2.setCursor(85, 60);
+        u8g2.print(lightDetect(LIGHT_SENSOR_PIN), 1); //显示环境光强
+    } while (u8g2.nextPage());
 }
